5bf9226a0342a1e7f686a86167d5a2c6de586926
iougemini <163802753+iougemini@users.noreply.github.com>
2024-05-28 16:50:53 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Dooy/chatgpt-web-midjourney-proxy/main/src/assets/avatar.jpg',
-      name:  t('mjset.sysname'),//'AI绘图',
-      description: 'Star on <a href="https://github.com/Dooy/chatgpt-web-midjourney-proxy" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'https://bbs.nies.live/uploads/default/optimized/1X/40585fd41e6156b114fa647d53278aaeb74043b0_2_180x180.png',
+      name:  t('mjset.sysname'),//'Niege Chat',
+      description: '© <a href="https://bbs.nies.live" class="" target="_blank" >Niege AI Community</a>',
     },
   }
 }
